9dc2e35a537b0f564183c5fed97a84387aa5005b
The Xen The <mlafzi@gmail.com>
2025-05-29 16:22:08 +0330

    Update tready.sh

--- tready.sh
+++ tready.sh
@@ -1,3 +1,7 @@
 pkg update
 pkg upgrade
 pgk install python3
+mkdir env00
+python -m venv env00
+source ./env00/bin/activate
+sudo apt-get install python-dev libsdl-image1.2-dev libsdl-mixer1.2-dev libsdl-ttf2.0-dev libsdl1.2-dev libsmpeg-dev python-numpy subversion libportmidi-dev ffmpeg libswscale-dev libavformat-dev libavcodec-dev libfreetype6-dev
